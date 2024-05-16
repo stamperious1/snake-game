a2cf67b05016483977363966b0c26df23b19c2b6
Rob Stamper <stamperious@gmail.com>
2024-05-16 17:39:26 -0400

    added slicing for segments list to simplify logic

--- main.py
+++ main.py
@@ -42,10 +42,9 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect collision with snake body
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    # snake.segments[1:] slices the list to give everything but the first item
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
